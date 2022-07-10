5e42da5c70cda8a72a6b86f6fdc4049af7ead06c
Kate <katensullivan55@gmail.com>
2022-07-10 18:18:17 -0400

    made login js and css

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import About from './components/About';
 import Footer from './components/Footer';
+import Login from './components/Login'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Login />
       <Sidebar />
       <About />
       <Footer />
