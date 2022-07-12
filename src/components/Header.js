a01bb031c3882f02f9fd502c34b52165119d8e24
Kate <katensullivan55@gmail.com>
2022-07-12 14:08:49 -0400

    routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import Login from "./containers/Login";
 
 
 export default function Header() {
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,79 @@
-import React from 'react';
-import './Login.css';
+import React, { useState } from "react";
 
+import Form from "react-bootstrap/Form";
+
+import Button from "react-bootstrap/Button";
+
+import "./Login.css";
 
 export default function Login() {
-    return (
-        <link rel="stylesheet" href="Login.css">
-        <form action="action_page.php" method="post">
-            <div class="imgcontainer">
-                <img src="img_avatar2.png" alt="Avatar" class="avatar"/>
-            </div>
 
-            <div class="container">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required/>
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required/>
-                <button type="submit">Login</button>
-                <label>
-                <input type="checkbox" checked="checked" name="remember"/> Remember me
-                </label>
-            </div>
+  const [email, setEmail] = useState("");
 
-            <div class="container">
-                <button type="button" class="cancelbtn">Cancel</button>
-                <span class="psw">Forgot password?</span>
-            </div>
-        </form>
-    )
-};
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+
+    return email.length > 0 && password.length > 0;
+
+  }
+
+  function handleSubmit(event) {
+
+    event.preventDefault();
+
+  }
+
+  return (
+
+    <div className="Login">
+
+      <Form onSubmit={handleSubmit}>
+
+        <Form.Group size="lg" controlId="email">
+
+          <Form.Label>Email</Form.Label>
+
+          <Form.Control
+
+            autoFocus
+
+            type="email"
+
+            value={email}
+
+            onChange={(e) => setEmail(e.target.value)}
+
+          />
+
+        </Form.Group>
+
+        <Form.Group size="lg" controlId="password">
+
+          <Form.Label>Password</Form.Label>
+
+          <Form.Control
+
+            type="password"
+
+            value={password}
+
+            onChange={(e) => setPassword(e.target.value)}
+
+          />
+
+        </Form.Group>
+
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+
+          Login
+
+        </Button>
+
+      </Form>
+
+    </div>
+
+  );
+
+}
