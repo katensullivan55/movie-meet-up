8ee16fba8be4af6a82a775c6bef609c0353b36b0
Kate <katensullivan55@gmail.com>
2022-07-12 14:59:46 -0400

    routing login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import About from './components/About';
 import Footer from './components/Footer';
-import Login from './components/Login'
+import Login from './components/Login/Login';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 
 
@@ -13,11 +17,30 @@ import Login from './components/Login'
 function App() {
   return (
     <div className="App">
+      
       <Header />
       <Login />
       <Sidebar />
       <About />
       <Footer />
+    </div>
+  );
+}
+
+function Wrapper() {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/preferences">
+          <Preferences />
+        </Route>
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
