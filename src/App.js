afb5c0d98e18da104643b3de0b5186f2f7d2c011
Kate <katensullivan55@gmail.com>
2022-07-12 19:24:21 -0400

    slight change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import logo from './logo.svg';
+//import { BrowserRouter, Route, Switch } from 'react-router-dom';
+//import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import About from './components/About';
 import Footer from './components/Footer';
 import Login from './containers/Login'
-// import Dashboard from './components/Dashboard/Dashboard';
-// import Preferences from './components/Preferences/Preferences';
+//import Dashboard from './components/Dashboard/Dashboard';
+//import Preferences from './components/Preferences/Preferences';
 
 
 
