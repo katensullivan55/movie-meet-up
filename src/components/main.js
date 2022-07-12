cd825926a3966eacb1b059646d625a79a398a974
SamMcPeak <97133188+SamMcPeak@users.noreply.github.com>
2022-07-12 18:25:14 -0400

    changed style for main catagory

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './main.css';
+
+
+export default function Header() {
+    return (
+        <div className='main'>
+            <h1>Movie 1</h1>
+            <h1>Movie 2</h1>
+            <h1>Movie 3</h1>
+            <h1>Movie 4</h1>
+            <h1>Movie 5</h1>
+            <h1>Movie 6</h1>
+        </div>
+    )
+};
