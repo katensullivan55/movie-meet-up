89fd9891cb56260573dbab714fc7f1b2f6cd475a
Kate <katensullivan55@gmail.com>
2022-07-14 00:00:14 -0400

    changing background

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,9 +3,11 @@ import './Movie.css'
 
 const Movie = ({movie}) => {
     return (
-        <div class='movieTile'>
-            <h5 class='movieName'>{movie.title}</h5>
-            <img class='movieImg' src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt={movie.path} />
+        <div class='movieTile polaroid'>
+            <a>
+                <h5 class='movieName'>{movie.title}</h5>
+                <img class='movieImg' src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} alt={movie.path} />
+            </a>
         </div>
       );
 }
